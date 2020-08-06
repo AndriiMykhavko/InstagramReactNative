ec45a131c948dec89ac91d53bed2991ee445b782
Andrii Mykhavko <58447850+AndriiMykhavko@users.noreply.github.com>
2020-08-06 14:04:36 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import 'react-native-gesture-handler';
+import React from 'react';
+import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+class App extends React.Component{
+  render() {
+    return(
+      <NavigationContainer>
+        <Text>Hello</Text>
+      </NavigationContainer>
+    )
+  }
+}
+
+export default App;
